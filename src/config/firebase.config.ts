1732e1a08874c7234d4db8ce13a9c04a1973ddb3
dyah-eka <dyahekalestari9@gmail.com>
2023-09-17 20:38:56 +0700

    fix(firebase-config): separate env variable for firebase private key

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -4,6 +4,9 @@ import { credential } from 'firebase-admin';
 
 export const setupFirebase = (config: ConfigService) => {
   const credentialJson = JSON.parse(config.get<string>('FIREBASE_JSON'));
+  credentialJson.private_key = config
+    .get<string>('FIREBASE_PRIVATE_KEY')
+    .replace(/\\n/g, '\n');
 
   initializeApp({
     credential: credential.cert(credentialJson),
