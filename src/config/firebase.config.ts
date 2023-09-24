0f1e67afa4c3ecc2f7f68dcb6fbf79b2a3da1379
dyah-eka <dyahekalestari9@gmail.com>
2023-09-25 05:49:53 +0700

    fix(firebase): fix private key format wrong on production

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -3,10 +3,9 @@ import { initializeApp } from 'firebase-admin/app';
 import { credential } from 'firebase-admin';
 
 export const setupFirebase = (config: ConfigService) => {
+  const privateKey = JSON.parse(config.get<string>('FIREBASE_PRIVATE_KEY'));
   const credentialJson = JSON.parse(config.get<string>('FIREBASE_JSON'));
-  credentialJson.private_key = config
-    .get<string>('FIREBASE_PRIVATE_KEY')
-    .replace(/\\n/g, '\n');
+  credentialJson.private_key = privateKey.private_key?.replace(/\\n/g, '\n');
 
   initializeApp({
     credential: credential.cert(credentialJson),
